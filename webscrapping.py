5c5ed189c6866e0c9e0357168e03e89d63710665
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-15 00:42:01 +0100

    Implement threading and improve a lot of details

--- webscrapping.py
+++ webscrapping.py
@@ -1,36 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Comando en IONOS
-# 0 4 * * * /usr/bin/python3 /kunden/homepages/11/d875513567/htdocs/personal.jofaval.com/webscrapping/big-data-aplicado/webscrapping.py
-# Comandos de testeo del cron (https://askubuntu.com/questions/85558/verify-if-crontab-works#answer-85610)
-# 1. /usr/bin/python3 /kunden/homepages/11/d875513567/htdocs/personal.jofaval.com/webscrapping/big-data-aplicado/webscrapping.py
-# 2. /bin/sh -c "(export PATH=/usr/bin/python3; /kunden/homepages/11/d875513567/htdocs/personal.jofaval.com/webscrapping/big-data-aplicado/webscrapping.py </dev/null)"
-# 3. /bin/sh -c "(export PATH=/usr/bin/python3; /kunden/homepages/11/d875513567/htdocs/personal.jofaval.com/webscrapping/big-data-aplicado/webscrapping.py </dev/null >/dev/null 2>&1)"
-
-MODULES = [
-    'requests',
-    'bs4',
-    'validators',
-    'cchardet',
-    'lxml',
-]
-def import_or_install(package):
-    try:
-        __import__(package)
-    except ImportError:
-        pip.main(['install', package]) 
-        __import__(package)
-
-# Instala/Importa todos los módulos necesarios
-[import_or_install(module) for module in MODULES]
-
-# Para las peticiones
 import requests
 # Para el parseo del HTML
 from bs4 import BeautifulSoup
 # Para la representación visual de los datos, usado anteriormente para guardar en CSV
-# import pandas as pd
+import pandas as pd
 # Para las operaciones de sistema, limpiar consola y crear directorios
 import os
 # Para validar la URL de las peticiones
@@ -62,8 +34,6 @@ from requests.models import Response
 # |               CONSTANTES               |
 # |----------------------------------------|
 
-basedir = os.path.dirname(__file__)
-# basedir = os.path.join('kunden', basedir, 'webscrapping', 'big-data-aplicados')
 true = True
 false = False
 
@@ -96,20 +66,32 @@ DOWNLOAD_ATTEMPTS = 3
 # Si no se quiere volver a intentar los intentos pasan a ser 1 (la primera vez)
 if not RETRY: DOWNLOAD_ATTEMPTS = 1
 
+MODULES = [
+    'validators',
+    'bs4',
+    'cchardet'
+]
 
 # Acelera las solicitud de peticiones HTTP al reusar la sesión que se abre en el primer request
 # cada request de normal crea una nueva sesión, de esta manera se utilizará siempre la misma
 # https://thehftguy.com/2020/07/28/making-beautifulsoup-parsing-10-times-faster/
 REQUEST_SESSION = requests.Session()
 
-# Controla el uso de Threads en el sistema
-USE_THREADS = False
-
 # |-----------------------------------------|
 # |                 MÉTODOS                 |
 # |-----------------------------------------|
 
 def is_null(val): return val is None
+
+def import_or_install(package):
+    try:
+        __import__(package)
+    except ImportError:
+        pip.main(['install', package]) 
+        __import__(package)
+
+# Instala/Importa todos los módulos necesarios
+[import_or_install(module) for module in MODULES]
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -122,10 +104,10 @@ def create_folder_if_not_exists(folder):
 
 create_folder_if_not_exists(IMG_DIR)
 
-def is_success(res):
+def is_success(res: Response):
     return not (res.status_code < 200 | res.status_code >= 300)
 
-def get_page(url, display = True):
+def get_page(url: str, display: bool = True):
     if DEBUG: print('Se va a recuperar el contenido de la página', url)
 
     if not (validators.url(url)):
@@ -145,7 +127,7 @@ def get_page(url, display = True):
         return html
 
 EMPTY_VALUE = " "
-def add_detail(dict, name, value = None):
+def add_detail(dict: dict, name: str, value: any = None):
     if is_null(value): value = EMPTY_VALUE
     dict[name] = value
 
@@ -167,28 +149,17 @@ def parse_img(element):
 
     return src
 
-def parse_price_prev(element, default = '£0.00'):
-    if not element: return default
-
-    price_prev = element[0].text
-
-    price_prev = price_prev.replace('(Was ')
-    price_prev = price_prev.replace(')')
-
-    return price_prev
-
 FIELDS = {
-    'name'       : { 'query': '.product-main__name'                 , 'parser': None            , 'default': 'Sin nombre'           },
-    'price'      : { 'query': '.product-action__price'              , 'parser': None            , 'default': '£0.00'                },
-    'price_prev' : { 'query': '.product-offer__price'               , 'parser': parse_price_prev, 'default': '£0.00'                },
-    'description': { 'query': '.product-main__description'          , 'parser': None            , 'default': 'Sin descripción'      },
-    'img'        : { 'query': '.product-main__image-container img'  , 'parser': parse_img       , 'default': 'Imagen no encontrada' },
-    'category'   : { 'query': '.breadcrumb__list'                   , 'parser': parse_category  , 'default': 'Sin categoría'        },
-    'unit'       : { 'query': '.product-main__data'                 , 'parser': None            , 'default': 'Sin unidad'           },
-    'rating'     : { 'query': '.review-overview__rating.star-rating', 'parser': None            , 'default': 'Sin valoración'       },
+    'name'       : { 'query': '.product-main__name'                 , 'parser': None          , 'default': 'Sin nombre'           },
+    'price'      : { 'query': '.product-action__price'              , 'parser': None          , 'default': '£0.00'                },
+    'description': { 'query': '.product-main__description'          , 'parser': None          , 'default': 'Sin descripción'      },
+    'img'        : { 'query': '.product-main__image-container img'  , 'parser': parse_img     , 'default': 'Imagen no encontrada' },
+    'category'   : { 'query': '.breadcrumb__list'                   , 'parser': parse_category, 'default': 'Sin categoría'        },
+    'unit'       : { 'query': '.product-main__data'                 , 'parser': None          , 'default': 'Sin unidad'           },
+    'rating'     : { 'query': '.review-overview__rating.star-rating', 'parser': None          , 'default': 'Sin valoración'       },
 }
 
-def get_value(field, element):
+def get_value(field: dict, element):
     # Si el campo tiene una función de parseo, se utiliza
     if (('parser' in field) & (not is_null(field['parser']))):
         value = field['parser'](element)
@@ -204,7 +175,7 @@ def get_value(field, element):
     if not is_null(value): value = str(value).strip()
     return value
 
-def get_detail(soup, dictionary, name):
+def get_detail(soup, dictionary: dict, name: str):
     # print('Se intenta recueprar el campo', name)
 
     field = FIELDS[name] if name in FIELDS else " "
@@ -217,7 +188,7 @@ def get_detail(soup, dictionary, name):
 
     return value
 
-def get_content(url = None, content = None):
+def get_content(url: str = None, content: str = None):
     if not is_null(url): content = get_page(url, False)
     if is_null(content): return None
 
@@ -225,14 +196,14 @@ def get_content(url = None, content = None):
 
 URL_ID_START = '/p/'
 URL_ID_END = '/'
-def get_url_id(url):
+def get_url_id(url: str):
     start = url.find(URL_ID_START) + len(URL_ID_START)
     end = url.rfind(URL_ID_END)
     id = url[start:end]
 
     return id
 
-def get_product_details(url = None, content = None):
+def get_product_details(url: str = None, content: str = None):
     content = get_content(url, content)
     if is_null(content): return None
     if DEBUG: print('Se van a recuperar los detalles de una página de producto', url)
@@ -253,7 +224,7 @@ def is_category_url(url):
     return str(url).startswith('/c')
 
 CATEGORY_LINK_QUERY = 'a.subnav__link, a.navbar__link'
-def get_categories(url = None, content = None):
+def get_categories(url: str = None, content: str = None):
     content = get_content(url, content)
     if is_null(content): return None
     if DEBUG: print('Se recuperan las categorías')
@@ -267,11 +238,11 @@ def get_categories(url = None, content = None):
     if DEBUG: print('Se han recuperado un total de:', len(categories), 'categoría(s)')
     return categories
 
-def is_product_url(url):
+def is_product_url(url: str):
     return str(url).startswith('/p')
 
 CATEGORY_PRODUCT_LINK_QUERY = 'a.product-card'
-def get_category_products(url = None, content = None):
+def get_category_products(url: str = None, content: str = None):
     content = get_content(url, content)
     if is_null(content): return None
     if DEBUG: print('Se recuperan los productos de la categoría')
@@ -294,13 +265,13 @@ FILE_NEWLINE = "\n"
 DATA_FOLDER = 'data'
 EXTRA_START_COLUMNS = ['id']
 EXTRA_END_COLUMNS = ['url']
-def save_csv(data):
+def save_csv(data: list):
     if DEBUG: print('Se guarda el contenido en un fichero .csv', FILENAME)
     # keys = ['id'] + list(FIELDS.keys())
     keys = EXTRA_START_COLUMNS + list(FIELDS.keys()) + EXTRA_END_COLUMNS
     # keys = list(FIELDS.keys())
 
-    filename = os.path.join(basedir, DATA_FOLDER, FILENAME)
+    filename = os.path.join(DATA_FOLDER, FILENAME)
     create_folder_if_not_exists(DATA_FOLDER)
     with open(filename, 'w+', 2, 'utf-8') as file:
         file.write(SEPARATOR.join(['\"' + k + '\"' for k in keys]))
@@ -311,11 +282,11 @@ def save_csv(data):
             file.write(SEPARATOR.join(values))
             file.write(FILE_NEWLINE)
 
-def get_image_filename(url):
+def get_image_filename(url: str):
     return str(url).replace(BASE_IMG_URL, '').replace('/', '_')
 
 MAX_IMG_DOWNLOAD_ATTEMPTS = DOWNLOAD_ATTEMPTS
-def download_img(url):
+def download_img(url: str):
     for attempt in range(0, MAX_IMG_DOWNLOAD_ATTEMPTS):
         try:
             filename = get_image_filename(url)
@@ -329,7 +300,7 @@ def download_img(url):
 
             break
         except:
-            log_error('No se ha podido descargar la imagen ' + str(url) + ', ' + str(attempt + 1) + 'º intento')
+            log_error(f'No se ha podido descargar la imagen {url}, {attempt + 1}º intento')
 
 def get_now():
     return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -343,7 +314,7 @@ def log_error(data):
         log.write(get_timestamp_str() + ' ' + str(data))
         log.write(FILE_NEWLINE)
 
-def save_images(data):
+def save_images(data: list):
     if DEBUG: print('Se empiezan a descargar todas las imágenes de los productos')
 
     no_img_download = []
@@ -390,9 +361,9 @@ def wait_for_all_threads(empty = True, warning = True):
 THREADS = []
 # 10 va genial, 15 empieza a ser demasiado, y 6 demasiado poco
 # WORKERS
-THREADS_LIMIT = 25
+THREADS_LIMIT = 50
 
-def threadify(target, args):
+def threadify(target, args: tuple):
     # Guard close para evitar errores si solo se pasa un parámetro
     # if type(args) != tuple: args = tuple(args)
 
@@ -416,19 +387,18 @@ def product_to_thread(data):
             details.append(product_detail)
             break
         except:
-            log_error('No se ha podido descargar el producto ' + str(product) + ', ' + str(attempt + 1) + 'º intento')
+            log_error(f'No se ha podido descargar el producto {product}, {attempt + 1}º intento')
 
 MAX_CATEGORY_PRODUCTS_DOWNLOAD_ATTEMPTS = DOWNLOAD_ATTEMPTS
-# def category_products_to_thread(products: set, category_url, product_limit, limit):
+# def category_products_to_thread(products: set, category_url: str, product_limit: int, limit: int):
 def category_products_to_thread(data):
     products, category_url, product_limit, limit = data
 
-    product_urls = []
     for attempt in range(0, MAX_CATEGORY_PRODUCTS_DOWNLOAD_ATTEMPTS):
         try:
             product_urls = get_category_products(category_url)
         except:
-            log_error('No se han podido conseguir los productos de la categoría ' + str(category_url) + ', ' + str(attempt + 1) + 'º intento')
+            log_error(f'No se han podido conseguir los productos de la categoría {category_url}, {attempt + 1}º intento')
 
     num_products = len(products)
     if (limit > 0) & (num_products >= limit): return None
@@ -442,9 +412,9 @@ def category_products_to_thread(data):
 
     return None
 
-def is_csv_corrupt(filename):
+def is_csv_corrupt(filename: str):
     # Si no es CSV, se fuerza
-    if not str(filename).endswith('.csv'): filename = filename + '.csv'
+    if not str(filename).endswith('.csv'): filename = f'{filename}.csv'
     file_path = os.path.join(DATA_FOLDER, filename)
 
     with open(file_path, 'r+') as fr:
@@ -459,15 +429,15 @@ def is_csv_corrupt(filename):
 
 ALL = -1
 def scrape(
-    category_limit = ALL,
-    product_limit = ALL,
-    limit = ALL,
-    as_csv = True,
-    save_imgs = True,
-    display = False,
-    category_urls = None,
-    products = None,
-    detect_corruption = True
+    category_limit: int = ALL,
+    product_limit: int = ALL,
+    limit: int = ALL,
+    as_csv: bool = True,
+    save_imgs: bool = True,
+    display: bool = False,
+    category_urls: list = None,
+    products: list = None,
+    detect_corruption: bool = True
 ):
     start_time = time.time()
 
@@ -491,7 +461,7 @@ def scrape(
     # El limit no funciona con un solo elemento
     if len(category_urls) > 1 & limit != ALL:
         category_urls = category_urls[:category_limit]
-
+    
     # if DEBUG: print('\nRecuperando los productos de las categorías\n', len(category_urls), 'categoría(s)\n')
     print('\nRecuperando los productos de las categorías\n', len(category_urls), 'categoría(s)\n')
 
@@ -500,16 +470,10 @@ def scrape(
     if was_products_empty:
         categories_args = [(products, category_url, product_limit, limit) for category_url in category_urls]
 
-        if not USE_THREADS:
-            # Versión sin hilos
-            [category_products_to_thread(data) for data in categories_args]
-        else:
-            # Versión con hilos
-            with ThreadPoolExecutor(THREADS_LIMIT) as executor:
-                executor.map(category_products_to_thread, categories_args)
-                executor.shutdown()
+        with ThreadPoolExecutor(THREADS_LIMIT) as executor:
+            executor.map(category_products_to_thread, categories_args)
+            executor.shutdown()
 
-    # print('category_urls', products, categories_args)
     # if DEBUG: print('\nRecuperando los detalles de los productos\n', len(products), 'producto(s)\n')
     print('\nRecuperando los detalles de los productos\n', len(products), 'producto(s)\n')
 
@@ -518,26 +482,71 @@ def scrape(
 
     products_args = [(details, p) for p in products]
 
-    if not USE_THREADS:
-        # Versión sin hilos
-        [product_to_thread(data) for data in products_args]
-    else:
-        # Y luego ya se recorren cada uno de los productos recuperando su información
-        with ThreadPoolExecutor(THREADS_LIMIT) as executor:
-            # print('se está intentando', products_args)
-            executor.map(product_to_thread, products_args)
-            executor.shutdown()
+    # Y luego ya se recorren cada uno de los productos recuperando su información
+    with ThreadPoolExecutor(THREADS_LIMIT) as executor:
+        # print('se está intentando', products_args)
+
+        executor.map(product_to_thread, products_args)
+        executor.shutdown()
 
     if display:
-        print(details)
-        # df = pd.DataFrame(details)
-        # print(df)
+        df = pd.DataFrame(details)
+        print(df)
         # df.to_csv(FILENAME, SEPARATOR, EMPTY_VALUE)
 
     if as_csv:
         # if DEBUG: print('\nSe guarda como CSV\n', len(details), 'línea(s)\n')
         print('\nSe guarda como CSV\n', len(details), 'línea(s)\n')
         save_csv(details)
+    if save_imgs:
+        if DEBUG: print('\nSe descargan las imágenes\n')
+        save_images(details)
+    if detect_corruption:
+        is_corrupt = is_csv_corrupt(FILENAME)
+        is_corrupt_str = '' if is_corrupt else 'no'
+        print('El archivo', is_corrupt_str, 'es corrupto')
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    timedelta = format_time(total_time)
+    print('Tiempo total de ejecución:', timedelta)
+
+def format_time(seconds: float):
+    return datetime.timedelta(seconds=seconds)
+
+category_urls = [
+    
+]
+
+with open('categories.txt', 'r+') as f:
+    category_urls = [ line.strip() for line in f.readlines() ]
+
+# Introducir los productos que no se han podido descargar en la iteración
+# TODO: implementar un append al CSV cuando se pasen productos como argumento
+products = [
+    'https://www.thewhiskyexchange.com/p/19887/high-west-campfire',
+    'https://www.thewhiskyexchange.com/p/62334/kurayoshi-pure-malt',
+]
+
+# DEBUG = True
+DEBUG = False
+# 0:01:51.676625 con el debug a true   544 lineas
+# 0:01:41.111771 con el debug a false  522 lineas
+scrape(limit = ALL, save_imgs=False, display=False, category_urls=category_urls, detect_corruption=False)
+
+# TODO:
+# mirar cómo implementar la paginación para que me funcione bien
+# mejorar el sistema de threadify para que no sea tan abrupto y se vaya rellenando y vaciando automáticamente
+  # https://stackoverflow.com/questions/35160417/threading-queue-working-example
+  # https://gist.github.com/amirasaran/e91c7253c03518b8f7b7955df0e954bb # ejemplo de clase con callback al finalizar
+  # probar también a que el hilo vaya en otro lado, es decir, sea otro threading para llamar al threadify, y solo se pare en puntos críticos
+# arreglar el limit de los productos cuando se hace un threadify
+# hay un bug cuando category_urls no funciona con menos de dos elementos en el array si se pasa por parámetro, pasa en otros sitios también
+# hacer fichero con las categorías, con un paginate como opcional
+# fifo dos hilos separados y evento de cuando se añada un nuevo elemento
+# refactor de los DOWNLOAD_ATTEMPTS para que sea una única función la que lo contorle con los parámetros necesarios
+# tal vez se podría hacer que se hiciese una única función de búsqueda por CSS para agilizar el proceso
     if save_imgs:
         if DEBUG: print('\nSe descargan las imágenes\n')
         save_images(details)
